839ceeccb2c20b8ad2b74601d6c30fdfd10cb2ee
Gabriel Gallardo <profegallardo.gabriel@gmail.com>
2023-02-03 17:29:37 -0300

    Primera escritura de cv en repositorio

---
+++ proyecto_cv/function.js
@@ -0,0 +1,6 @@
+document.getElementById("boton").onmouseover = function () {
+    console.log("capturamos click");
+    document.getElementById("contacto").innerHTML = "Celular: 3865-656565 E-mail: contadorJR@GMAIL.COM";
+
+    
+}
